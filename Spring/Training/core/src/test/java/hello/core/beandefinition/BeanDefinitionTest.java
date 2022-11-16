c1898f0bd3efce89f280ed2c7d35d98a62837a1b
pjaehyun <show6756@gmail.com>
2022-11-16 22:36:18 +0900

    싱글톤 구성 확인

--- Spring/Training/core/src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
+++ Spring/Training/core/src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
@@ -12,7 +12,7 @@ public class BeanDefinitionTest {
 
 //    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
     GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
-    
+
 
     @Test
     @DisplayName("빈 설정 메타정보 확인")
---
+++ Spring/Training/core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,28 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer() {
+        AppConfig appConfig = new AppConfig();
+
+        MemberService memberService = appConfig.memberService();
+
+        MemberService memberService2 = appConfig.memberService();
+
+        System.out.println("memberService = " + memberService);
+        System.out.println("memberService2 = " + memberService2);
+
+        // memberService != memberService2
+        Assertions.assertThat(memberService).isNotSameAs(memberService2);
+
+
+    }
+}
