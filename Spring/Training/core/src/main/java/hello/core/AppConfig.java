920f66b9c7b5fee352bb35fdac5a6a2a4033c74c
pjaehyun <show6756@gmail.com>
2022-11-16 23:53:50 +0900

    싱글톤과 Configuration

--- Spring/Training/core/src/main/java/hello/core/AppConfig.java
+++ Spring/Training/core/src/main/java/hello/core/AppConfig.java
@@ -16,18 +16,35 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    // @Bean memberService -> new MemoryMemberRepository()
+    // @Bean orderService -> new MemoryMemberRepository()
+
+    // 예측 호출 순서
+    // call AppConfig.memberService
+    // call AppConfig.MemberRepository
+    // call AppConfig.MemberRepository
+    // call AppConfig.OrderService
+    // call AppConfig.MemberRepository
+
+    // 스프링 빈 호출 순서
+    // call AppConfig.memberService
+    // call AppConfig.memberRepository
+    // call AppConfig.orderService
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.MemberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.OrderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
