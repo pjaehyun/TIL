05271a873d4f782123fb2856378ce593188460a2
pjaehyun <show6756@gmail.com>
2022-11-16 23:29:02 +0900

    싱글톤 상태공유 문제 해결(무상태로 설계)

--- Spring/Training/core/src/test/java/hello/core/singleton/StatefulService.java
+++ Spring/Training/core/src/test/java/hello/core/singleton/StatefulService.java
@@ -2,14 +2,8 @@ package hello.core.singleton;
 
 public class StatefulService {
 
-    private int price;
-
-    public void order(String name, int price) {
+    public int order(String name, int price) {
         System.out.println("name = " + name + " price = " + price);
-        this.price = price;
-    }
-
-    public int getPrice() {
         return price;
     }
 }
