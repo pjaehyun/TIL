6f3edab92e8851bebd68981559db07aea2b0fe2d
pjaehyun <show6756@gmail.com>
2022-11-16 22:52:12 +0900

    싱글톤 패턴 테스트

--- Spring/Training/core/src/test/java/hello/core/singleton/SingletonTest.java
+++ Spring/Training/core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -6,6 +6,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class SingletonTest {
 
     @Test
@@ -21,8 +23,22 @@ public class SingletonTest {
         System.out.println("memberService2 = " + memberService2);
 
         // memberService != memberService2
-        Assertions.assertThat(memberService).isNotSameAs(memberService2);
+        assertThat(memberService).isNotSameAs(memberService2);
 
 
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        assertThat(singletonService1).isSameAs(singletonService2);
+        // same ==
+        // equal Java의 equals
     }
 }
