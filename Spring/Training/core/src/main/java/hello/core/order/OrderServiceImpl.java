d527668af4959ed9fd72a50d53559339a70c4f01
pjaehyun <show6756@gmail.com>
2022-11-22 23:46:02 +0900

    다양한 의존관계 주입 방법

--- Spring/Training/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ Spring/Training/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,8 +9,37 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService {
 
+//    필드 주입(잘 사용안함) -> 외부에서 변경이 불가능하여 테스트하기 힘들다.
+//    @Autowired private MemberRepository memberRepository;
+//    @Autowired private DiscountPolicy discountPolicy;
+
+//    수정자 주입
+//    private MemberRepository memberRepository;
+//    private DiscountPolicy discountPolicy;
+
+//    @Autowired
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+//
+//    @Autowired
+//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//        this.discountPolicy = discountPolicy;
+//    }
+
+//    일반 메서드 주입
+//    private MemberRepository memberRepository;
+//    private DiscountPolicy discountPolicy;
+//    @Autowired
+//    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy
+//    }
+
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+
+
 
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
