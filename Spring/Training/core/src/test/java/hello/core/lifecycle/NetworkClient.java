c395dbd168f06686d4568065b3b921392441fb52
pjaehyun <show6756@gmail.com>
2022-11-27 23:42:38 +0900

    빈 생명주기 초기화(@PostConstruct, @PreDestroy)

--- Spring/Training/core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ Spring/Training/core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,7 +1,7 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 public class NetworkClient {
     private String url;
@@ -28,12 +28,14 @@ public class NetworkClient {
         System.out.println("close " + url);
     }
 
+    @PostConstruct
     public void init() {
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
+    @PreDestroy
     public void close() throws Exception {
         System.out.println("NetworkClient.init");
         disconnect();
