f0425e3293e41b231c8cc2ad7c49e772f8f4b558
pjaehyun <show6756@gmail.com>
2022-12-01 20:28:34 +0900

    request 스코프 with 프록시

--- Spring/Training/core/src/main/java/hello/core/web/LogDemoController.java
+++ Spring/Training/core/src/main/java/hello/core/web/LogDemoController.java
@@ -14,13 +14,12 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
--- Spring/Training/core/src/main/java/hello/core/web/LogDemoService.java
+++ Spring/Training/core/src/main/java/hello/core/web/LogDemoService.java
@@ -9,9 +9,8 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class LogDemoService {
 
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
     public void logic(String id) {
-        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("service id = " + id);
     }
 }
