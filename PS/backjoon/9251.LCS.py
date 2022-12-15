c7e36f4b606bf737c5d7265c670efa845dfcf51e
pjaehyun <show6756@gmail.com>
2022-12-15 16:20:49 +0900

    20221215 문제 풀이

---
+++ PS/backjoon/9251.LCS.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+str1 = input().strip()
+str2 = input().strip()
+
+n, m = len(str1), len(str2)
+
+dp = [[0] * (m+1) for _ in range(n+1)]
+
+for i in range(1, n+1):
+    for j in range(1, m+1):
+        if str1[i-1] == str2[j-1]:
+            dp[i][j] = dp[i-1][j-1] + 1
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+print(dp[-1][-1])
