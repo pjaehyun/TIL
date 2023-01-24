41c4825ab2f421624d530972aa9335d62e176cba
pjaehyun <show6756@gmail.com>
2023-01-24 22:05:49 +0900

    상품 수정 컨트롤러, 화면 개발

--- Spring/Training/jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
+++ Spring/Training/jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -25,7 +27,12 @@ public class ItemController {
 
     @PostMapping("/items/new")
     public String create(BookForm form) {
-        Book book = Book.createBook(form.getName(), form.getPrice(), form.getStockQuantity(), form.getAuthor(), form.getIsbn());
+        Book book = new Book();
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
         itemService.saveItem(book);
         return "redirect:/items";
     }
@@ -36,4 +43,37 @@ public class ItemController {
         model.addAttribute("items", items);
         return "items/itemList";
     }
+
+    @GetMapping("/items/{itemId}/edit")
+    public String updateItemForm(@PathVariable("itemId") Long itemId, Model model) {
+        Book item = (Book) itemService.findOne(itemId);
+
+        BookForm form = new BookForm();
+        form.setId(item.getId());
+        form.setName(item.getName());
+        form.setPrice(item.getPrice());
+        form.setStockQuantity(item.getStockQuantity());
+        form.setAuthor(item.getAuthor());
+        form.setIsbn(item.getIsbn());
+
+        model.addAttribute("form", form);
+        return "items/updateItemForm";
+    }
+
+    @PostMapping("/items/{itemId}/edit")
+    public String updateItem(@PathVariable String itemId, @ModelAttribute("form") BookForm form) {
+
+        Book book = new Book();
+        book.setId(form.getId());
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/items";
+
+    }
+
 }
--- Spring/Training/jpashop/src/main/java/jpabook/jpashop/domain/Item/Book.java
+++ Spring/Training/jpashop/src/main/java/jpabook/jpashop/domain/Item/Book.java
@@ -1,8 +1,6 @@
 package jpabook.jpashop.domain.Item;
 
-import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.DiscriminatorValue;
@@ -10,21 +8,9 @@ import javax.persistence.Entity;
 
 @Entity
 @DiscriminatorValue("B")
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Setter
 public class Book extends Item {
     private String author;
     private String isbn;
-
-    public static Book createBook(String name, int price, int stockQuantity, String author, String isbn) {
-        Book book = new Book();
-        book.setName(name);
-        book.setPrice(price);
-        book.setStockQuantity(stockQuantity);
-        book.setAuthor(author);
-        book.setIsbn(isbn);
-
-        return book;
-    }
 }
