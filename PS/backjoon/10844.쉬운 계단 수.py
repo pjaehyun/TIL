2e3d3ba7f04f4b6db877e472de93aa6caae2808f
pjaehyun <show6756@gmail.com>
2023-02-24 16:37:35 +0900

    20230224 문제 풀이

---
+++ PS/backjoon/10844.쉬운 계단 수.py
@@ -0,0 +1,11 @@
+N = int(input())
+
+dp = [[0] * 12 for _ in range(N+1)]
+
+for i in range(2, 11):
+    dp[1][i] = 1
+
+for i in range(2, N+1):
+    for j in range(1, 11):
+        dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
+print(sum(dp[N]) % 1000000000)
