af3c81d27ecbdbfd037967f6992f4c0339002860
pjaehyun <show6756@gmail.com>
2023-03-26 12:24:49 +0900

    문제 풀이 코드 추가

--- PS/programmers/바탕화면 정리.py
+++ PS/programmers/바탕화면 정리.py
@@ -1,5 +1,21 @@
+# 첫번째 풀이
 def solution(wallpaper):
     u, d, l, r = len(wallpaper)-1, 0, len(wallpaper[0])-1, 0
+    for i in range(len(wallpaper)):
+        for j in range(len(wallpaper[0])):
+            if wallpaper[i][j] == '#':
+                if i < u:
+                    u = i
+                if i > d:
+                    d = i
+                if j < l:
+                    l = j
+                if j > r:
+                    r = j
+    return [u, l, d+1, r+1]
+
+# 두번째 풀이(코드 개선)
+def solution(wallpaper):
     x, y = [], []
     for i in range(len(wallpaper)):
         for j in range(len(wallpaper[0])):
