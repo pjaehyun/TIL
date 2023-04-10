9efff89d3070200643eec9fc160a57439f2aa84c
pjaehyun <show6756@gmail.com>
2023-04-10 18:44:15 +0900

    20230410 문제 풀이

--- PS/leetcode/20.Valid Parentheses.py
+++ PS/leetcode/20.Valid Parentheses.py
@@ -1,3 +1,4 @@
+# 첫번째 풀이
 class Solution:
     def isValid(self, s: str) -> bool:
         s = list(s)
@@ -13,6 +14,25 @@ class Solution:
                 s.pop(0)
             else: 
                 stack.append(s.pop(0))
+        if stack:
+            return False
+        return True
+
+# 두번째 풀이
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+
+        dq = deque(list(s))
+        while dq:
+            curr = dq.popleft()
+            if not stack:
+                stack.append(curr)
+            elif stack[-1] == '(' and curr == ')' or stack[-1] == '[' and curr == ']' or stack[-1] == '{' and curr == '}':
+                stack.pop()
+                continue
+            else:
+                stack.append(curr)
         if stack:
             return False
         return True
