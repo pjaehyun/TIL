789a24d743e26f7b9d82179edd239af306fbaa8f
pjaehyun <show6756@gmail.com>
2023-04-25 23:37:06 +0900

    풀이 추가

--- PS/leetcode/2336.Smallest Number in Infinite Set.py
+++ PS/leetcode/2336.Smallest Number in Infinite Set.py
@@ -1,3 +1,4 @@
+# 첫번째 풀이
 class SmallestInfiniteSet:
 
     def __init__(self):
@@ -13,6 +14,28 @@ class SmallestInfiniteSet:
     def addBack(self, num: int) -> None:
         if num not in self.hq:
             heapq.heappush(self.hq, num)
+
+# 두번째 풀이(딕셔너리를 이용한 시간복잡도 개선)
+class SmallestInfiniteSet:
+
+    def __init__(self):
+        self.hq = []
+        self.isExist = {}
+        self.num = 1
+
+    def popSmallest(self) -> int:
+        if self.hq:
+            min = heapq.heappop(self.hq)
+            self.isExist[min] = 0
+            return min
+        self.num += 1
+        return self.num - 1
+        
+
+    def addBack(self, num: int) -> None:
+        if self.num > num and self.isExist.get(num, 0) == 0:
+            heapq.heappush(self.hq, num)
+            self.isExist[num] = 1
         
 
 
