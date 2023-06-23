f22867928a2d8e75aa69ce0d5aad47a2907c97a5
pjaehyun <show6756@gmail.com>
2023-06-23 14:22:42 +0900

    풀이 방식 추가

--- PS/leetcode/1027.Longest Arithmetic Subsequence.py
+++ PS/leetcode/1027.Longest Arithmetic Subsequence.py
@@ -1,3 +1,4 @@
+# 첫번째 풀이
 class Solution:
     def longestArithSeqLength(self, nums: List[int]) -> int:
         n = len(nums)
@@ -11,3 +12,16 @@ class Solution:
                 dp[i][diff] = dp[j][diff] + 1
                 answer = max(answer, dp[i][diff])
         return answer + 1
+
+# 두번째 풀이
+class Solution:
+    def longestArithSeqLength(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n <= 2: return 2
+        dp = defaultdict(int)
+        
+        for i in range(n):
+            for j in range(i+1, n):
+                diff = nums[i] - nums[j]
+                dp[(j, diff)] = (dp[(i, diff)] + 1)
+        return max(dp.values()) + 1
