0874a63c38ac553302d3ef3e5654b3b3dfe20017
pjaehyun <show6756@gmail.com>
2023-06-28 18:15:02 +0900

    로그아웃 구현(Redis사용)

---
+++ Spring/Training/jwt/src/main/java/com/jh/jwt/configuration/RedisProperties.java
@@ -0,0 +1,14 @@
+package com.jh.jwt.configuration;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "spring.redis")
+@Data
+public class RedisProperties {
+
+    private int port;
+    private String host;
+}
