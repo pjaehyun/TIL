40be50e6b61727eb91f719b9505ef400f13356df
pjaehyun <show6756@gmail.com>
2023-07-04 16:34:37 +0900

    풀이 코드 추가

--- PS/leetcode/137.Single Number II.py
+++ PS/leetcode/137.Single Number II.py
@@ -1,3 +1,4 @@
+# 첫번째 풀이(완전 탐색)
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         count = defaultdict(int)
@@ -9,3 +10,14 @@ class Solution:
             if v == 1:
                 return k
         return -1
+
+# 두번째 풀이(비트연산 풀이 참고)
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        ones = 0
+        twos = 0
+
+        for num in nums:
+            ones = (ones^num) & (~twos)
+            twos = (twos^num) & (~ones)
+        return ones
