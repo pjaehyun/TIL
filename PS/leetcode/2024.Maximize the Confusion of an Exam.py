8afec6506d6a25e2e96f052506cb7d7842d74cd9
pjaehyun <show6756@gmail.com>
2023-07-10 19:23:19 +0900

    코드 수정

--- PS/leetcode/2024.Maximize the Confusion of an Exam.py
+++ PS/leetcode/2024.Maximize the Confusion of an Exam.py
@@ -32,3 +32,26 @@ class Solution:
             answer = max(answer, j - i)
 
         return answer
+    
+# 두번째 풀이(정답 참고)
+class Solution:
+    def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
+        
+        t, f = 0, 0
+        answer = 0
+        l = 0
+        for r in range(len(answerKey)):
+            if answerKey[r] == 'F':
+                f += 1
+            else:
+                t += 1
+        
+            while min(t, f) > k:
+                if answerKey[l] == 'T':
+                    t -= 1
+                else:
+                    f -= 1
+                l += 1
+            answer = max(answer, r - l + 1)
+        return answer
+        
