8bb23aeecf4a804eb4d2e92a0102f37071b7183d
pjaehyun <show6756@gmail.com>
2023-07-13 21:44:26 +0900

    풀이 코드 추가

--- PS/leetcode/802.Find Eventual Safe States.py
+++ PS/leetcode/802.Find Eventual Safe States.py
@@ -1,3 +1,4 @@
+# 첫번째 풀이(Dfs)
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
         n = len(graph)
@@ -30,3 +31,28 @@ class Solution:
             if safe[i] == 1:
                 answer.append(i)
         return answer
+    
+# 두번째 풀이(Topological Sort)
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        c_graph = {x:[] for x in range(len(graph))}
+        in_degree = {x:0 for x in range(len(graph))}
+        for i in range(len(graph)):
+            for j in graph[i]:
+                c_graph[j].append(i)
+                in_degree[i] += 1
+        
+        dq = deque()
+        for k, v in in_degree.items():
+            if v == 0:
+                dq.append(k)
+        answer = []
+        while dq:
+            curr = dq.popleft()
+            answer.append(curr)
+            
+            for neib in c_graph[curr]:
+                in_degree[neib] -= 1
+                if in_degree[neib] == 0:
+                    dq.append(neib)
+        return sorted(answer)
