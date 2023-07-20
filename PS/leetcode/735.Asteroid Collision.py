14fe62a9244e7f8d23f9d33110fbe8ac28815e71
pjaehyun <show6756@gmail.com>
2023-07-20 11:30:08 +0900

    코드 수정

--- PS/leetcode/735.Asteroid Collision.py
+++ PS/leetcode/735.Asteroid Collision.py
@@ -4,7 +4,7 @@ class Solution:
         
         for i in range(1, len(asteroids)):
             while stack and stack[-1] > 0 and asteroids[i] < 0:
-                if stack[-1] < abs(asteroids[i]):
+                if stack[-1] + asteroids[i] < 0:
                     stack.pop()
                 elif stack[-1] + asteroids[i] == 0:
                     stack.pop()
