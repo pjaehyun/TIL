de984be438d82f0f3c78f9aec0b4e9b0e0d2828a
pjaehyun <show6756@gmail.com>
2023-10-13 10:29:48 +0900

    풀이 추가

--- PS/leetcode/746.Min Cost Climbing Stairs.py
+++ PS/leetcode/746.Min Cost Climbing Stairs.py
@@ -1,3 +1,4 @@
+# 첫번째 풀이
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         cost = cost + [0]
@@ -5,3 +6,13 @@ class Solution:
         for i in range(2, len(cost)):
             cost[i] = min(cost[i-2] + cost[i], cost[i-1] + cost[i])
         return cost[-1]
+    
+# 두번째 풀이
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        dp = [0] * len(cost)
+
+        dp[-1], dp[-2] = cost[-1], cost[-2]
+        for i in range(len(dp) - 3, -1, -1):
+            dp[i] = cost[i] + min(dp[i+1], dp[i+2])
+        return min(dp[0], dp[1])
