924066b2485f0979d562b027fb9cffd72bc0ce4b
pjaehyun <show6756@gmail.com>
2024-01-01 14:13:58 +0900

    다른 풀이 추가

--- PS/leetcode/455.Assign Cookies.py
+++ PS/leetcode/455.Assign Cookies.py
@@ -1,3 +1,4 @@
+# 첫번째 풀이
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
         g.sort()
@@ -9,3 +10,18 @@ class Solution:
                 i, result = i + 1, result + 1
             j = j + 1
         return result
+
+# 두번째 풀이
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        s.sort()
+
+        answer = 0
+        heapq.heapify(g)
+        i = 0
+        while g and i < len(s):
+            if g[0] <= s[i]:
+                answer += 1
+                heapq.heappop(g)
+            i += 1
+        return answer
