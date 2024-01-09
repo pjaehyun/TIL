767c92bb032cca0c1c0deed2efa72605d9f203c3
pjaehyun <show6756@gmail.com>
2024-01-09 15:12:52 +0900

    20230109 문제 풀이

---
+++ PS/backjoon/1002.터렛.py
@@ -0,0 +1,19 @@
+import sys
+import math
+input = sys.stdin.readline
+
+t = int(input())
+
+for _ in range(t):
+    x1, y1, r1, x2, y2, r2 = map(int, input().split())
+
+    distance = math.sqrt((x1-x2)**2 + (y1-y2)**2)
+
+    if distance == 0 and r1 == r2:
+        print(-1)
+    elif abs(r1-r2) == distance or r1+r2 == distance:
+        print(1)
+    elif abs(r1-r2) < distance < (r1+r2):
+        print(2)
+    else:
+        print(0)
