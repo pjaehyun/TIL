b3e3e3221fdb4d17a6d72030fe80d0dcbdd460b1
pjaehyun <show6756@gmail.com>
2024-01-21 13:43:32 +0900

    20230121 문제 풀이

--- PS/leetcode/198.House Robber.py
+++ PS/leetcode/198.House Robber.py
@@ -1,3 +1,4 @@
+# 첫번째 풀이
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
@@ -9,3 +10,18 @@ class Solution:
         for i in range(3, n+1):
             dp[i] = max(nums[i-1] + dp[i-2], nums[i-1] + dp[i-3])
         return max(dp)
+    
+# 두번째 풀이
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        if n == 1:
+            return nums[0]
+        
+        dp = [0] * (n + 1)
+        dp[1] = nums[0]
+
+        for i in range(2, n+1):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i-1])
+        return max(dp[-1], dp[-2])
