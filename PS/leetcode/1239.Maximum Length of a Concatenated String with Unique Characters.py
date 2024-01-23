d7d544c42785eb00a85288a4dca497275f9963a9
pjaehyun <show6756@gmail.com>
2024-01-23 13:50:11 +0900

    20230123 문제 풀이

--- PS/leetcode/1239.Maximum Length of a Concatenated String with Unique Characters.py
+++ PS/leetcode/1239.Maximum Length of a Concatenated String with Unique Characters.py
@@ -1,3 +1,4 @@
+# 첫번째 풀이
 class Solution:
     def maxLength(self, arr: List[str]) -> int:
         result = [""]
@@ -9,3 +10,39 @@ class Solution:
                     result.append(temp)
         result.sort(key=lambda x:len(x))
         return len(result[-1])
+
+# 두번째 풀이
+class Solution:
+    def maxLength(self, arr: List[str]) -> int:
+         answer = ""
+
+         def unique (s):
+             comp = defaultdict(int)
+
+             for i in range(len(s)):
+                 if comp[s[i]] >= 1:
+                     return False
+                 comp[s[i]] += 1
+             return True
+
+         def bt(i, concat):
+             nonlocal answer 
+             if len(concat) > len(answer):
+                 answer = concat
+            
+             for j in range(i+1, len(arr)):
+                 if j not in visited and unique(concat + arr[j]):
+                     visited.add(j)
+                     bt(j, concat + arr[j])
+                     visited.remove(j)
+        
+         visited = set()
+
+         for i in range(len(arr)):
+             if unique(arr[i]):
+                visited.add(i)
+                bt(i, arr[i])
+                visited.remove(i)
+         return len(answer)
+
+        
