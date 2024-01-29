913b79196296b7800cc01accc9c57e5f329ba840
pjaehyun <show6756@gmail.com>
2024-01-29 13:00:23 +0900

    풀이 추가

--- PS/leetcode/232.Implement Queue using Stacks.py
+++ PS/leetcode/232.Implement Queue using Stacks.py
@@ -1,4 +1,5 @@
 # 문제 요구사항: 두개의 스택을 이용하여 문제를 해결하라
+# 첫번째 풀이
 class MyQueue:
 
     def __init__(self):
@@ -22,3 +23,28 @@ class MyQueue:
 
     def empty(self) -> bool:
         return not self.stack1 and not self.stack2
+    
+# 두번쨰 풀이 
+class MyQueue:
+
+    def __init__(self):
+        self.s1 = []
+        self.s2 = []
+
+    def push(self, x: int) -> None:
+        while self.s1:
+            self.s2.append(self.s1.pop())
+        self.s1.append(x)
+        while self.s2:
+            self.s1.append(self.s2.pop())
+
+    def pop(self) -> int:
+        return self.s1.pop()
+
+    def peek(self) -> int:
+        return self.s1[-1]
+
+    def empty(self) -> bool:
+        if not self.s1:
+            return True
+        return False
