bb3f33618680efe10c8403907ddac572238fecae
pjaehyun <show6756@gmail.com>
2024-02-22 20:32:59 +0900

    문제 풀이 추가

--- PS/leetcode/997.Find the Town Judge.py
+++ PS/leetcode/997.Find the Town Judge.py
@@ -1,3 +1,4 @@
+# 첫번째 풀이
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
         
@@ -10,5 +11,18 @@ class Solution:
         
         for i in range(1, n+1):
             if graph[i][0] == 0 and graph[i][1] == n-1:
+                return i
+        return -1
+    
+# 두번째 풍리 
+class Solution:
+    def findJudge(self, N: int, trust: List[List[int]]) -> int:
+        in_degree = [0] * (N + 1)
+        out_degree = [0] * (N + 1)
+        for a in trust:
+            out_degree[a[0]] += 1
+            in_degree[a[1]] += 1
+        for i in range(1, N + 1):
+            if in_degree[i] == N - 1 and out_degree[i] == 0:
                 return i
         return -1
