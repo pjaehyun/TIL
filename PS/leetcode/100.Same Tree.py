eca8522bf69a097f34a1b026aaf28553b7ca6d2e
pjaehyun <show6756@gmail.com>
2024-02-26 20:55:19 +0900

    풀이 추가

--- PS/leetcode/100.Same Tree.py
+++ PS/leetcode/100.Same Tree.py
@@ -4,6 +4,7 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# 첫번째 풀이
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         p_nodes = []
@@ -21,3 +22,12 @@ class Solution:
         if p_nodes == q_nodes:
             return True
         return False
+    
+# 두번째 풀이
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
