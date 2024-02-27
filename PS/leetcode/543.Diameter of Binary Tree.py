36612572530aa47be474f7ddd5705bbd3affe168
pjaehyun <show6756@gmail.com>
2024-02-27 11:18:50 +0900

    20230227 문제 풀이

---
+++ PS/leetcode/543.Diameter of Binary Tree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        def dfs(root):
+            if not root:
+                return 0
+            left = dfs(root.left)
+            right = dfs(root.right)
+            dist[0] = max(dist[0], left + right)
+            return max(left, right) + 1
+        dist = [0]
+        dfs(root)
+        return dist[0]
