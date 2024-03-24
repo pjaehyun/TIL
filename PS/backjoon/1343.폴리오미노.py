c6c79e67d852eb6bf215fc220f7cf096581da56b
pjaehyun <show6756@gmail.com>
2024-03-24 15:22:20 +0900

    20240324 문제 풀이

---
+++ PS/backjoon/1343.폴리오미노.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+board = input().strip()
+board = board.replace("XXXX", "AAAA")
+board = board.replace("XX", "BB")
+
+if 'X' in board:
+    print(-1)
+else:
+    print(board)
