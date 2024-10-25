b0f7017c29520f761ad94513e1ee9824a977b908
pjaehyun <show6756@gmail.com>
2024-10-25 17:37:25 +0900

    코드 수정

--- PS/leetcode/1233.Remove Sub-Folders from the Filesystem.py
+++ PS/leetcode/1233.Remove Sub-Folders from the Filesystem.py
@@ -1,14 +1,8 @@
-class Solution(object):
-    def removeSubfolders(self, folder):
-        folder.sort(key=len)
-        result = set()
-        for dir in folder:
-            dir_parts = dir.split('/')
-            parent_dir = ''
-            for part in dir_parts[1:]:
-                parent_dir += "/" + part
-                if parent_dir in result:
-                    break
-            else:
-                result.add(parent_dir)
-        return list(result)
+class Solution:
+    def removeSubfolders(self, folder: List[str]) -> List[str]:
+        folder.sort()
+        result = []
+        for f in folder:
+            if not result or not f.startswith(result[-1] + '/'):
+                result.append(f)
+        return result
