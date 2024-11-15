3e33252ea938ce6ada379fd135964503e3cf13c2
pjaehyun <show6756@gmail.com>
2024-11-15 23:47:44 +0900

    20241115 문제 풀이

---
+++ PS/leetcode/1574.Shortest Subarray to be Removed to Make Array Sorted.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findLengthOfShortestSubarray(self, arr: List[int]) -> int:
+        n = len(arr)
+        
+        left = 0
+        while left + 1 < n and arr[left] <= arr[left + 1]:
+            left += 1
+        
+        if left == n - 1:
+            return 0
+        
+        right = n - 1
+        while right > 0 and arr[right - 1] <= arr[right]:
+            right -= 1
+        
+        result = min(n - left - 1, right)
+        
+        i, j = 0, right
+        while i <= left and j < n:
+            if arr[i] <= arr[j]:
+                result = min(result, j - i - 1)
+                i += 1
+            else:
+                j += 1
+        
+        return result
--- PS/leetcode/leetcode.md
+++ PS/leetcode/leetcode.md
@@ -718,3 +718,4 @@
 - 2070\. Most Beautiful Item for Each Query 문제 풀이 완료(20241112)
 - 2563\. Count the Number of Fair Pairs 문제 풀이 완료(20241113)
 - 2064\. Minimized Maximum of Products Distributed to Any Store 문제 풀이 완료(20241114)
+- 1574\. Shortest Subarray to be Removed to Make Array Sorted 문제 풀이 완료(20241115)
