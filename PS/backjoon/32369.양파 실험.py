c0de6c24ca64f735016aa0f3843c2b03886f2eb4
pjaehyun <show6756@gmail.com>
2026-02-09 10:28:50 +0900

    20260209 문제 풀이

---
+++ PS/backjoon/32369.양파 실험.py
@@ -0,0 +1,22 @@
+import sys
+import heapq, math
+from collections import defaultdict, deque, Counter
+input = sys.stdin.readline
+
+N, A, B = map(int, input().split())
+x, y = 1, 1
+ 
+while N:
+    x += A
+    y += B
+ 
+    if x < y:
+        x, y = y, x
+ 
+    if x == y:
+        y -= 1
+ 
+    N -= 1
+ 
+ 
+print(x, y)
