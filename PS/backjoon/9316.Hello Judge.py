3b5abb98e2e86230eb2eb3e323e7eb5a1e77c97f
pjaehyun <show6756@gmail.com>
2026-02-18 10:41:55 +0900

    20260218 문제 풀이

---
+++ PS/backjoon/9316.Hello Judge.py
@@ -0,0 +1,10 @@
+import sys
+import heapq, math
+from collections import defaultdict, deque, Counter
+input = sys.stdin.readline
+
+
+n = int(input())
+
+for i in range(1, n+1):
+    print("Hello World, Judge {}!".format(i))
